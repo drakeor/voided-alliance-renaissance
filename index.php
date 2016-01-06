08e7afbad46a68ddcf45a8565a51794c36fd4dae
Drakeor <drakeor.dragon@gmail.com>
2016-01-06 00:49:04 -0800

    More  bootstrap changes

--- index.php
+++ index.php
@@ -174,19 +174,19 @@ wti($pagenum);
     </nav>
 
     <!-- Main jumbotron for a primary marketing message or call to action -->
-     <div class="jumbotron">
+     <!--<div class="jumbotron">
       <div class="container">
         <h1>Hello, world!</h1>
         <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
         <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
       </div>
-    </div>
+    </div>-->
 
     <div class="container">
       <!-- Example row of columns -->
       <div class="row">
         <div class="col-md-9">
-          <h2>Heading</h2>
+          <h2>Main Content</h2>
           <p>
 <?php
 if (empty($_COOKIE['va_users']) == false) {
@@ -7362,9 +7362,13 @@ cpl_wra();
 </p>
        </div>
         <div class="col-md-3">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <h2>Navigation</h2>
+          <p>
+		  <?php writeleft(); ?>
+		  </p>
+		  <p>
+		  <?php writebottom(); ?>
+		  </p>
         </div>
       </div>
 
